9bf8609c1dd751e53c005f42ffc35c005b5d2786
limodou <limodou@gmail.com>
2021-12-26 14:35:26 +0800

    优化结构

--- rapi-server/src/auth/controller.rs
+++ rapi-server/src/auth/controller.rs
@@ -3,63 +3,23 @@ use axum::{extract::Extension, http::StatusCode, response::IntoResponse, Json};
 use sqlx::{self, mysql::MySqlPool};
 
 use super::User;
-use crate::core::{result, Res, AppError};
 use super::{LoginUser, RegisterUser};
+use crate::core::{result, AppError, Res};
 
 pub async fn login(
   Json(user): Json<LoginUser>,
   Extension(pool): Extension<MySqlPool>,
 ) -> Result<impl IntoResponse, AppError> {
-  let user = User::find_by_username(&pool, &user.username).await?;
+  User::find_by_username(&pool, &user.username).await?;
   Ok(result::ok())
-  // match User::find_by_username(&pool, &user.username).await {
-  //   Ok(user) => {
-  //     println!("{:?}", user);
-
-  //     let res = Res::<String> {
-  //       code: 0,
-  //       data: Some("".into()),
-  //       message: None,
-  //     };
-
-  //     Ok((StatusCode::OK, Json(res)))
-  //   }
-  //   Err(e) => {
-  //     let res = Res::<String> {
-  //       code: 1,
-  //       data: None,
-  //       message: Some(e.to_string()),
-  //     };
-  //     (StatusCode::OK, Json(res))
-  //   }
-  // }
 }
 
 pub async fn register(
   Json(user): Json<RegisterUser>,
   Extension(pool): Extension<MySqlPool>,
-) -> impl IntoResponse {
+) -> Result<impl IntoResponse, AppError> {
   println!("user {:?}", user);
 
-  match User::create(&pool, &user).await {
-    Ok(_) => {
-      println!("{:?}", user);
-
-      let res = Res::<String> {
-        code: 0,
-        data: Some("".into()),
-        message: None,
-      };
-
-      (StatusCode::CREATED, Json(res))
-    }
-    Err(e) => {
-      let res = Res::<String> {
-        code: 1,
-        data: None,
-        message: Some(e.to_string()),
-      };
-      (StatusCode::OK, Json(res))
-    }
-  }
+  User::create(&pool, &user).await?;
+  Ok(result::ok())
 }
--- rapi-server/src/core/error.rs
+++ rapi-server/src/core/error.rs
@@ -4,7 +4,6 @@ use axum::{
   http::StatusCode,
   response::{IntoResponse, Response},
 };
-// use std::fmt;
 use super::result;
 
 #[derive(Debug)]
@@ -14,34 +13,7 @@ pub struct AppError {
   pub message: String,
 }
 
-// impl fmt::Display for AppError {
-//   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//     write!(f, "({}, {}, {})", self.status_code, self.code, self.message)
-//   }
-// }
-
-// impl Error for AppError {
-//   fn description(&self) -> &str {
-//     self.message.as_str()
-//   }
-// }
-
-// impl<E> From<E> for AppError
-// where
-//   E: std::error::Error,
-// {
-//   fn from(error: E) -> AppError {
-//     AppError {
-//       status_code: StatusCode::INTERNAL_SERVER_ERROR,
-//       code: 1,
-//       message: error.to_string(),
-//     }
-//   }
-// }
-
 impl From<anyhow::Error> for AppError
-// where
-//   E: std::error::Error,
 {
   fn from(error: anyhow::Error) -> AppError {
     AppError {
@@ -57,9 +29,3 @@ impl IntoResponse for AppError {
     result::fail_status_code(self.status_code, self.code, &self.message).into_response()
   }
 }
-
-// impl IntoResponse for anyhow::Error {
-//   fn into_response(self) -> Response {
-//     result::fail_status_code(StatusCode::OK, 999, &self.to_string()).into_response()
-//   }
-// }
